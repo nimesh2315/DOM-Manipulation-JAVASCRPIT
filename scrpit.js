b9abcd504f1106442c81a3085edf96208ec5876e
Nimesh Shrivastava <116950933+nimesh2315@users.noreply.github.com>
2023-08-03 11:11:06 +0530

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,68 @@
+
+// 4  Pillar of DOM
+
+// 1. SELECTION OF DOM
+// 2  CHANGING IN HTML
+// 3. CHANGING IN CSS
+// 4. EVENT LISTENER
+
+
+
+//var a = document.querySelector("h1")
+//console.log(a)
+//a.style.color ="red"
+//a.style.backgroundColor ="black"
+
+
+
+//var a = document.querySelector("h1")
+//a.innerHTML ="TECHNOCRATS INSTITUTE OF TECHNOLOGY BHOPAL"
+//a.style.color ="YELLOW"
+//a.style.backgroundColor ="#000"
+
+//var a = document.querySelector("h1")
+
+//a.addEventListener("click",function(){
+   // a.innerHTML="Badal gya hu mai bhi teri ex ki tarah"
+    //a.style.color ="yellow"
+    //a.style.backgroundColor="#000"
+//})
+
+
+// var bulb = document.querySelector("#bulb")
+// var button = document.querySelector("button")
+
+// var flag = 0
+
+// button.addEventListener("click",function(){
+//     if (flag == 0){
+//     bulb.style.backgroundColor = "yellow"
+//     console.log("cliocked")
+//     flag = 1
+//     }else{
+//     bulb.style.backgroundColor = "transparent"
+//     console.log("again cliocked")
+//     flag = 0
+//     }
+
+// })
+
+// var h = document.querySelectorAll("h1")
+// console.log(h)
+
+// h.forEach(function(e){
+//     console.log(e)
+// })
+
+
+
+// document.getElementById("Box")
+
+// document.getElementsByClassName("Box")
+
+
+// var box = document.querySelector("#box")
+// box.innerHTML = "<h1>HEELO</h1>"
+
+// var box = document.querySelector("#box")
+//  box.textContent = "<h1>HEELO</h1>"
